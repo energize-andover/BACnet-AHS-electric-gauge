83aefa783bd425918ea1a078f417bede496496fa
Ayush Zenith <ayushp.zenith@gmail.com>
2018-11-26 18:51:25 -0500

    Python API update Website pre-loaders

--- GaugeProjectApache/py/Main.py
+++ GaugeProjectApache/py/Main.py
@@ -2,38 +2,37 @@ import pygal
 import argparse
 import pandas as pd
 import time
+from building_data_requests import get_value
+import numbers
 from datetime import timedelta
 import datetime
-from bacnet_gateway_requests import get_value_and_units
 
 def myFunction(facility):
     try:
-        # Get hostname and port of BACnet Gateway
-        parser = argparse.ArgumentParser(description='Test BACnet Gateway', add_help=False)
-        parser.add_argument('-h', dest='hostname')
-        parser.add_argument('-p', dest='port')
-        args = parser.parse_args()
-
         # Read spreadsheet into a dataframe.
         # Each row contains the following:
-        #   - Feeder
+        #   - Label
+        #   - Facility
         #   - Instance ID of electric meter
-        df = pd.read_csv('../csv/ahs_elec.csv')
+        df = pd.read_csv('../csv/ahs_power.csv')
+
+        # Output column headings
+        print('Feeder,Meter,Units')
 
         # Iterate over the rows of the dataframe, getting meter readings for each feeder
+        # Iterate over the rows of the dataframe, getting meter readings for each feeder
         for index, row in df.iterrows():
-            if facility!=row['Label']:
-                continue
             # Retrieve data
-            value, units = get_value_and_units(row['Facility'], row['Meter'], args.hostname, args.port)
+            value, units = get_value(row['Facility'], row['Meter'])
 
             # Prepare to print
-            value = int(value) if value else ''
+            value = int(value) if isinstance(value, numbers.Number) else ''
 
             # Output CSV format
             if facility == row['Label']:
                 return (value)
-                print(value)
+                print(facility+value)
+                print(test)
 
     except KeyboardInterrupt:
         print('Bye')
@@ -42,22 +41,22 @@ def myFunction(facility):
 gauge_chart = pygal.Gauge(human_readable=True)
 
 mainkWhConstant=myFunction("Main (kWh)")
-if mainkWhConstant=='':
+while mainkWhConstant=='' or mainkWhConstant is None:
     mainkWhConstant=0
 mainkWhConstant=int(mainkWhConstant)
 
 gymkWhConstant = myFunction("DG (kWh)")
-if gymkWhConstant == '':
+while gymkWhConstant == '' or gymkWhConstant is None:
     gymkWhConstant = 0
 gymkWhConstant = int(gymkWhConstant)
 
 kitchenkWhConstant = myFunction("DE (kWh)")
-if kitchenkWhConstant == '':
+while kitchenkWhConstant == '' or kitchenkWhConstant is None:
     kitchenkWhConstant = 0
 kitchenkWhConstant = int(kitchenkWhConstant)
 
 collinscenterkWhConstant = myFunction("AMDP (kWh)")
-if collinscenterkWhConstant == '':
+while collinscenterkWhConstant == '' or collinscenterkWhConstant is None:
     collinscenterkWhConstant = 0
 collinscenterkWhConstant = int(collinscenterkWhConstant)
 
@@ -85,45 +84,45 @@ try:
         currentDT2 = datetime.date.today()
 
         mainkW=myFunction("Main (kW)")
-        if mainkW=='':
+        if mainkW=='' or mainkW is None:
             mainkW=0
         mainkW=int(mainkW)
 
         mainkWh=myFunction("Main (kWh)")
-        if mainkWh=='':
+        if mainkWh=='' or mainkWh is None:
             mainkWh=0
         mainkWh=int(mainkWh)
         mainkWh=mainkWh-mainkWhConstant
 
         gymkW = myFunction("DG (kW)")
-        if gymkW == '':
+        if gymkW == '' or gymkW is None:
             gymkW = 0
         gymkW = int(gymkW)
 
         gymkWh = myFunction("DG (kWh)")
-        if gymkWh == '':
+        if gymkWh == '' or gymkWh is None:
             gymkWh = 0
         gymkWh = int(gymkWh)
         gymkWh=gymkWh-gymkWhConstant
 
         kitchenkW = myFunction("DE (kW)")
-        if kitchenkW == '':
+        if kitchenkW == '' or kitchenkW is None:
             kitchenkW = 0
         kitchenkW = int(kitchenkW)
 
         kitchenkWh = myFunction("DE (kWh)")
-        if kitchenkWh == '':
+        if kitchenkWh == '' or kitchenkWh is None:
             kitchenkWh = 0
         kitchenkWh = int(kitchenkWh)
         kitchenkWh=kitchenkWh-kitchenkWhConstant
 
         collinscenterkW = myFunction("AMDP (kW)")
-        if collinscenterkW == '':
+        if collinscenterkW == '' or collinscenterkW is None:
             collinscenterkW = 0
         collinscenterkW = int(collinscenterkW)
 
         collinscenterkWh = myFunction("AMDP (kWh)")
-        if collinscenterkWh == '':
+        if collinscenterkWh == '' or collinscenterkWh is None:
             collinscenterkWh = 0
         collinscenterkWh = int(collinscenterkWh)
         collinscenterkWh=collinscenterkWh-collinscenterkWh
@@ -166,93 +165,117 @@ try:
         dollar.render_to_file("dollars.svg")
         if currentDT.hour == 23:
             currentday = currentDT2.day+timedelta(days=1)
-        if currentDT2.day == currentday:
-            if currentDT.hour>=5 and currentDT.hour<16:
-                if (int(currentDT.hour)==6) and (six==False):
-                    six=True
-                    gauge_chart.add('6 am', 0)
-                    sixamkWh = myFunction("Main (kWh)")
-                    if sixamkWh == '':
+        try:
+            currentday
+        except NameError:
+            print("kWh hourly not ready yet")
+        else:
+            if currentDT2.day == currentday:
+                if currentDT.hour>=5 and currentDT.hour<16:
+                    if (int(currentDT.hour)==6) and (six==False):
+                        six=True
+                        gauge_chart.add('6 am', 0)
                         sixamkWh = myFunction("Main (kWh)")
-                    sixamkWh = int(sixamkWh)
+                        if sixamkWh == '' or sixamkWh is None:
+                            sixamkWh = myFunction("Main (kWh)")
+                        sixamkWh = int(sixamkWh)
 
-                if (int(currentDT.hour)==7) and (seven==False):
-                    seven=True
-                    sevenamkWh = myFunction("Main (kWh)")
-                    if sevenamkWh == '':
+                    if (int(currentDT.hour)==7) and (seven==False):
+                        seven=True
                         sevenamkWh = myFunction("Main (kWh)")
-                    sevenamkWh = int(sevenamkWh)
-                    gauge_chart.add('7 am', sevenamkWh-sixamkWh)
+                        if sevenamkWh == '' or sevenamkWh is None:
+                            sevenamkWh = myFunction("Main (kWh)")
+                        sevenamkWh = int(sevenamkWh)
+                        gauge_chart.add('7 am', sevenamkWh-sixamkWh)
 
-                if (int(currentDT.hour)==8) and (eight==False):
-                    eight=True
-                    eightamkWh = myFunction("Main (kWh)")
-                    if eightamkWh == '':
+                    if (int(currentDT.hour)==8) and (eight==False):
+                        eight=True
                         eightamkWh = myFunction("Main (kWh)")
-                    eightamkWh = int(eightamkWh)
-                    gauge_chart.add('8 am', eightamkWh-sixamkWh)
+                        if eightamkWh == '' or eightamkWh is None:
+                            eightamkWh = myFunction("Main (kWh)")
+                        eightamkWh = int(eightamkWh)
+                        gauge_chart.add('8 am', eightamkWh-sixamkWh)
 
-                if (int(currentDT.hour)==9) and (nine==False):
-                    nine=True
-                    nineamkWh = myFunction("Main (kWh)")
-                    if nineamkWh == '':
+                    if (int(currentDT.hour)==9) and (nine==False):
+                        nine=True
                         nineamkWh = myFunction("Main (kWh)")
-                    nineamkWh = int(nineamkWh)
-                    gauge_chart.add('9 am', nineamkWh-sixamkWh)
+                        if nineamkWh == '' or nineamkWh is None:
+                            nineamkWh = myFunction("Main (kWh)")
+                        nineamkWh = int(nineamkWh)
+                        gauge_chart.add('9 am', nineamkWh-sixamkWh)
 
-                if (int(currentDT.hour)==10) and (ten==False):
-                    ten=True
-                    tenamkWh = myFunction("Main (kWh)")
-                    if tenamkWh == '':
+                    if (int(currentDT.hour)==10) and (ten==False):
+                        ten=True
                         tenamkWh = myFunction("Main (kWh)")
-                    tenamkWh = int(tenamkWh)
-                    gauge_chart.add('10 am', tenamkWh-sixamkWh)
+                        if tenamkWh == '' or tenamkWh is None:
+                            tenamkWh = myFunction("Main (kWh)")
+                        tenamkWh = int(tenamkWh)
+                        gauge_chart.add('10 am', tenamkWh-sixamkWh)
 
-                if (int(currentDT.hour) == 11) and (eleven == False):
-                    eleven = True
-                    elevenamkWh = myFunction("Main (kWh)")
-                    if elevenamkWh == '':
+                    if (int(currentDT.hour) == 11) and (eleven == False):
+                        eleven = True
                         elevenamkWh = myFunction("Main (kWh)")
-                    elevenamkWh = int(elevenamkWh)
-                    gauge_chart.add('11 am', elevenamkWh - sixamkWh)
+                        if elevenamkWh == '' or elevenamkWh is None:
+                            elevenamkWh = myFunction("Main (kWh)")
+                        elevenamkWh = int(elevenamkWh)
+                        gauge_chart.add('11 am', elevenamkWh - sixamkWh)
 
-                if (int(currentDT.hour) == 12) and (twelve == False):
-                    twelve = True
-                    twelvepmkWh = myFunction("Main (kWh)")
-                    if twelvepmkWh == '':
+                    if (int(currentDT.hour) == 12) and (twelve == False):
+                        twelve = True
                         twelvepmkWh = myFunction("Main (kWh)")
-                    twelvepmkWh = int(twelvepmkWh)
-                    gauge_chart.add('12 pm', twelvepmkWh - sixamkWh)
+                        if twelvepmkWh == '' or twelvepmkWh is None:
+                            twelvepmkWh = myFunction("Main (kWh)")
+                        twelvepmkWh = int(twelvepmkWh)
+                        gauge_chart.add('12 pm', twelvepmkWh - sixamkWh)
 
-                if (int(currentDT.hour) == 13) and (one == False):
-                    one = True
-                    onepmkWh = myFunction("Main (kWh)")
-                    if onepmkWh == '':
+                    if (int(currentDT.hour) == 13) and (one == False):
+                        one = True
                         onepmkWh = myFunction("Main (kWh)")
-                    onepmkWh = int(onepmkWh)
-                    gauge_chart.add('1 pm', onepmkWh - sixamkWh)
+                        if onepmkWh == '' or onepmkWh is None:
+                            onepmkWh = myFunction("Main (kWh)")
+                        onepmkWh = int(onepmkWh)
+                        gauge_chart.add('1 pm', onepmkWh - sixamkWh)
 
-                if (int(currentDT.hour) == 14) and (two == False):
-                    two = True
-                    twopmkWh = myFunction("Main (kWh)")
-                    if twopmkWh == '':
+                    if (int(currentDT.hour) == 14) and (two == False):
+                        two = True
                         twopmkWh = myFunction("Main (kWh)")
-                    twopmkWh = int(twopmkWh)
-                    gauge_chart.add('2 pm', twopmkWh - sixamkWh)
+                        if twopmkWh == '' or twopmkWh is None:
+                            twopmkWh = myFunction("Main (kWh)")
+                        twopmkWh = int(twopmkWh)
+                        gauge_chart.add('2 pm', twopmkWh - sixamkWh)
 
-                if (int(currentDT.hour) == 15) and (three == False):
-                    three = True
-                    threepmkWh = myFunction("Main (kWh)")
-                    if threepmkWh == '':
+                    if (int(currentDT.hour) == 15) and (three == False):
+                        three = True
                         threepmkWh = myFunction("Main (kWh)")
-                    threepmkWh = int(threepmkWh)
-                    gauge_chart.add('3 pm', threepmkWh - sixamkWh)
+                        if threepmkWh == '' or threepmkWh is None:
+                            threepmkWh = myFunction("Main (kWh)")
+                        threepmkWh = int(threepmkWh)
+                        gauge_chart.add('3 pm', threepmkWh - sixamkWh)
 
-                gauge_chart.render_to_file("kWhHourly.svg")
+                    gauge_chart.render_to_file("kWhHourly.svg")
 
         time.sleep(15)
 
         if (int(currentDT.hour) == 23):
+            mainkWhConstant = myFunction("Main (kWh)")
+            while mainkWhConstant == '' or mainkWhConstant is None:
+                mainkWhConstant = myFunction("Main (kWh)")
+            mainkWhConstant = int(mainkWhConstant)
+
+            gymkWhConstant = myFunction("DG (kWh)")
+            while gymkWhConstant == '' or gymkWhConstant is None:
+                mainkWhConstant = myFunction("Main (kWh)")
+            gymkWhConstant = int(gymkWhConstant)
+
+            kitchenkWhConstant = myFunction("DE (kWh)")
+            while kitchenkWhConstant == '' or kitchenkWhConstant is None:
+                kitchenkWhConstant = 0
+            kitchenkWhConstant = int(kitchenkWhConstant)
+
+            collinscenterkWhConstant = myFunction("AMDP (kWh)")
+            while collinscenterkWhConstant == '' or collinscenterkWhConstant is None:
+                collinscenterkWhConstant = 0
+            collinscenterkWhConstant = int(collinscenterkWhConstant)
             six = False
             seven = False
             eight = False
---
+++ GaugeProjectApache/py/building_data_requests.py
@@ -0,0 +1,105 @@
+# Copyright 2018 Building Energy Gateway.  All rights reserved.
+
+import requests
+import json
+
+PUBLIC_HOSTNAME = 'energize.andoverma.us'
+INTERNAL_PORT = '8000'
+
+
+# Request present value and units for the supplied instance
+def get_value( facility, instance, gateway_hostname=None, gateway_port=None, live=False ):
+
+    value = None
+    units = None
+
+    if str( instance ).isdigit() and int( instance ) > 0:
+        # Instance appears to be valid
+
+        # Set up request arguments
+        args = {
+            'facility': facility,
+            'instance': instance
+        }
+
+        if live:
+            args['live'] = True
+
+        # Issue request to web service
+        gateway_rsp = post_request( gateway_hostname, gateway_port, args )
+
+        # Convert JSON response to Python dictionary
+        dc_rsp = json.loads( gateway_rsp.text )
+
+        # Extract instance response from the dictionary
+        dc_inst_rsp = dc_rsp['instance_response']
+
+        # Extract result from instance response
+        if ( dc_inst_rsp['success'] ):
+
+            dc_data = dc_inst_rsp['data']
+
+            if dc_data['success']:
+                value = dc_data['presentValue']
+                units = dc_data['units']
+
+    return value, units
+
+
+# Request multiple values from Building Energy Gateway
+def get_bulk( bulk_request, gateway_hostname=None, gateway_port=None ):
+
+    bulk_rsp = []
+
+    if isinstance( bulk_request, list ) and len( bulk_request ):
+
+        # Set up request arguments
+        args = {
+            'bulk': json.dumps( bulk_request )
+        }
+
+        # Issue request to web service
+        gateway_rsp = post_request( gateway_hostname, gateway_port, args )
+
+        # Extract result
+        bulk_rsp = json.loads( gateway_rsp.text )
+
+    return bulk_rsp
+
+
+# Post request to web service
+def post_request( gateway_hostname, gateway_port, args ):
+
+    # Normalize hostname
+    if not gateway_hostname:
+        gateway_hostname = PUBLIC_HOSTNAME
+
+    # Normalize port
+    gateway_port = str( gateway_port ) if gateway_port else ''
+
+    # Set SSL and port fragments
+    if gateway_hostname == PUBLIC_HOSTNAME:
+
+        if gateway_port:
+            s = ''
+            port = ':' + gateway_port
+        else:
+            s = 's'
+            port = ''
+
+    else:
+
+        s = ''
+
+        if gateway_port:
+            port = ':' + gateway_port
+        else:
+            port = ':' + INTERNAL_PORT
+
+    # Format URL
+    url = 'http' + s + '://' + gateway_hostname + port
+
+    # Post request
+    gateway_rsp = requests.post( url, data=args )
+
+    return gateway_rsp
