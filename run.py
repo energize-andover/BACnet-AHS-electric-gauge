abe0bbd27e1b3caa02fabacd5fc756292a04faba
Ayush Zenith <ayushp.zenith@gmail.com>
2019-03-30 23:12:45 -0400

    gitignore updated

--- run.py
+++ run.py
@@ -49,13 +49,13 @@ def main():
     #Add headers to both force latest IE rendering engine or Chrome Frame,
     #and also to cache the rendered page for 10 minutes.
 
-#    @app.after_request
-#    def add_header(r):
-#        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#        r.headers["Pragma"] = "no-cache"
-#        r.headers["Expires"] = "0"
-#        r.headers['Cache-Control'] = 'public, max-age=0'
-#        return r
+    @app.after_request
+    def add_header(r):
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
 
     @app.route(HOST_PREFIX + '/dollars')
     def dollars():
