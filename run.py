1704a3cfebdc42539af709142ef534c179b6d012
Ayush Zenith <ayushp.zenith@gmail.com>
2019-03-30 22:59:58 -0400

    commented out header/cache-control

--- run.py
+++ run.py
@@ -46,17 +46,16 @@ def main():
     def index():
         return redirect(HOST_PREFIX + "/kW")
 
-    @app.after_request
-    def add_header(r):
-        """
-        Add headers to both force latest IE rendering engine or Chrome Frame,
-        and also to cache the rendered page for 10 minutes.
-        """
-        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        r.headers["Pragma"] = "no-cache"
-        r.headers["Expires"] = "0"
-        r.headers['Cache-Control'] = 'public, max-age=0'
-        return r
+    #Add headers to both force latest IE rendering engine or Chrome Frame,
+    #and also to cache the rendered page for 10 minutes.
+
+#    @app.after_request
+#    def add_header(r):
+#        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#        r.headers["Pragma"] = "no-cache"
+#        r.headers["Expires"] = "0"
+#        r.headers['Cache-Control'] = 'public, max-age=0'
+#        return r
 
     @app.route(HOST_PREFIX + '/dollars')
     def dollars():
